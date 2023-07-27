3c079802b0d21dda8f4064c89c9f5a5ed5bd69b0
Motayassa <malovilyajob@gmail.com>
2023-07-27 17:41:43 +0300

    Add method date_in_future

--- task_05.py
+++ task_05.py
@@ -1,14 +1,36 @@
 '''
-Разработать метод date_in_future(integer), который вернет дату через integer дней.
-Если integer не является целым числом, то метод должен вывести текущую дату.
-Формат возвращаемой методом даты должен иметь следующий вид '01-01-2001
-22:33:44'.
+Разработать метод date_in_future(integer), который вернет дату через
+integer дней. Если integer не является целым числом,
+то метод должен вывести текущую дату. Формат возвращаемой методом даты
+должен иметь следующий вид '01-01-2001 22:33:44'.
 
 '''
 
 
-'''
-Тесты для примеров и проверки:
-date_in_future([]) # => текущая дата
-date_in_future(2) # => текущая дата + 2 дня
-'''
+import datetime
+from datetime import timedelta
+
+
+def date_in_future(integer):
+    '''
+    Возвращает дату через integer дней, если integer является
+    целым числом, иначе возвращает текущую дату
+
+    '''
+
+    dt = datetime.datetime.now()
+
+    # Возвращение текущей даты
+    if type(integer) is not int:
+        return dt.strftime('%m-%d-%Y %H:%M:%S')
+
+    # Возвращение даты через integer дней
+    if type(integer) is int:
+        days = timedelta(integer)  # число дней для прибавления к дате
+        new_data = dt + days
+        return new_data.strftime('%m-%d-%Y %H:%M:%S')
+
+
+# Тесты для примеров и проверки:
+print(date_in_future([]))  # => текущая дата
+print(date_in_future(2))  # => текущая дата + 2 дня
