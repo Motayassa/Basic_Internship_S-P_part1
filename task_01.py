379b7b706308255c3d477154d80a658e46937ad2
Motayassa <malovilyajob@gmail.com>
2023-07-27 12:10:17 +0300

    Add method is_palindrome in task_01.py

--- task_01.py
+++ task_01.py
@@ -2,11 +2,16 @@
 является ли параметр string палиндромом (строкой, которая читается
 одинаково как сначала так и с конца), при условии игнорирования пробелов,
 знаков препинания и регистра.'''
+from string import punctuation
 
 
-'''Тесты для примеров и проверки:
-is_palindrome("A man, a plan, a canal -- Panama") # => True
-is_palindrome("Madam, I'm Adam!") # => True
-is_palindrome(333) # => True
-is_palindrome(None) # => False
-is_palindrome("Abracadabra") # => False'''
+def is_palindrome(string):
+    '''Определяет является ли параметр палиндромом,
+    без учета знаков препинания, пробелов и регистра'''
+    if type(string) != str:
+        string = str(string)
+    string = [str(x) for x in string if x not in punctuation + ' ']
+    string = ''.join(string).lower()
+    if string == string[::-1]:
+        return True
+    return False
