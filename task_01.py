08e8bf56cee2e1fe997fbbd9e557b4e83021085c
Motayassa <malovilyajob@gmail.com>
2023-07-27 12:22:29 +0300

    Fix PEP8

--- task_01.py
+++ task_01.py
@@ -1,13 +1,19 @@
 '''Разработайте метод is_palindrome(string), который будет определять,
 является ли параметр string палиндромом (строкой, которая читается
 одинаково как сначала так и с конца), при условии игнорирования пробелов,
-знаков препинания и регистра.'''
+знаков препинания и регистра.
+
+'''
+
+
 from string import punctuation
 
 
 def is_palindrome(string):
     '''Определяет является ли параметр палиндромом,
-    без учета знаков препинания, пробелов и регистра'''
+    без учета знаков препинания, пробелов и регистра
+
+    '''
     if type(string) != str:
         string = str(string)
     string = [str(x) for x in string if x not in punctuation + ' ']
@@ -17,6 +23,7 @@ def is_palindrome(string):
     return False
 
 
+#  Tests
 a = is_palindrome("A man, a plan, a canal -- Panama")  # => True
 b = is_palindrome("Madam, I'm Adam!")  # => True
 c = is_palindrome(333)  # => True
