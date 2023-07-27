82a09e5de7ddb52d93ca0d0bb338bba2d7d0d4ec
Motayassa <malovilyajob@gmail.com>
2023-07-27 11:37:10 +0300

    Start task_01.py

---
+++ task_01.py
@@ -0,0 +1,12 @@
+'''Разработайте метод is_palindrome(string), который будет определять,
+является ли параметр string палиндромом (строкой, которая читается
+одинаково как сначала так и с конца), при условии игнорирования пробелов,
+знаков препинания и регистра.'''
+
+
+'''Тесты для примеров и проверки:
+is_palindrome("A man, a plan, a canal -- Panama") # => True
+is_palindrome("Madam, I'm Adam!") # => True
+is_palindrome(333) # => True
+is_palindrome(None) # => False
+is_palindrome("Abracadabra") # => False'''
