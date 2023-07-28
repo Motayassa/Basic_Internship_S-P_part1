5b0713496725a358d3e78275e78e6e8501b062c9
Motayassa <malovilyajob@gmail.com>
2023-07-28 16:21:45 +0300

    Start task_08.py

---
+++ task_08.py
@@ -0,0 +1,24 @@
+'''
+Написать метод multiply_numbers(inputs), который вернет произведение цифр,
+входящих в inputs.
+
+'''
+
+
+def multiply_numbers(inputs):
+    '''
+    Возвращает произведение цифр, входящих в inputs
+
+    '''
+    pass
+
+
+'''
+# Тест для примеров и проверки:
+multiply_numbers() # => None
+multiply_numbers('ss') # => None
+multiply_numbers('1234') # => 24
+multiply_numbers('sssdd34') # => 12
+multiply_numbers(2.3) # => 6
+multiply_numbers([5, 6, 4]) # => 120
+'''
