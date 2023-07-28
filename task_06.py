1b9194686b87b1b6ff31d077d3043219c561b9b4
Motayassa <malovilyajob@gmail.com>
2023-07-28 13:08:36 +0300

    Add auxiliary method gamer1_vs_gamer2

--- task_06.py
+++ task_06.py
@@ -37,6 +37,32 @@ def rps_game_winner(game):
     if gamer1[1] not in ('R', 'P', 'S') or gamer2[1] not in ('R', 'P', 'S'):
         raise NoSuchStrategyError('В игре участвуют только камень - R, ножницы - S и бумага - P')
 
+    res = gamer1_vs_gamer2(gamer1, gamer2)
+
+
+def gamer1_vs_gamer2(gamer1, gamer2):  # Вспомогательный метод
+    '''
+    Возращает победителя в игре камень-ножницы-бумага,
+    при ничьей побеждает игрок №1
+
+    '''
+    if (
+        ('R' in gamer1 and 'S' in gamer2) or
+        ('S' in gamer1 and 'P' in gamer2) or
+        ('P' in gamer1 and 'R' in gamer2)
+    ):
+        return gamer1
+
+    if gamer1[1] == gamer2[1]:
+        return gamer1
+
+    if (
+        ('R' in gamer2 and 'S' in gamer1) or
+        ('S' in gamer2 and 'P' in gamer1) or
+        ('P' in gamer2 and 'R' in gamer1)
+    ):
+        return gamer2
+
 
 # Тесты
 rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])  # => WrongNumberOfPlayersError
