e97f5408b50bac342383020abfe48995a6157bc8
Motayassa <malovilyajob@gmail.com>
2023-07-28 15:43:23 +0300

    Add method combine_anagrams and tests

--- task_07.py
+++ task_07.py
@@ -5,11 +5,35 @@
 Разработайте метод combine_anagrams(words_array), который принимает на вход
 массив слов и разбивает их в группы по анаграммам, регистр букв не имеет
 значения при определении анаграмм.
-Тест для примеров и проверки:
 
 '''
 
 
-'''combine_anagrams(["cars", "for", "potatoes", "racs", "four", "scar",
-"creams", "scream"]) # => [ ["cars", "racs", "scar"], ["four"], ["for"],
-["potatoes"], ["creams", "scream"] ]'''
+def combine_anagrams(words_array):
+    '''Принимает на вход массив слов и разбивает их в группы по анаграммам,
+    регистр букв не имеет значения при определении анаграмм.
+
+    '''
+
+    anagram_groups = []  # хранилище для групп анаграмм
+
+    for j in words_array:
+
+        # отфильтровываем анаграммы в список anagrams
+        anagrams = list(filter(lambda x: set(j) == set(x), words_array))
+
+        # сохраняем анаграммы в результирущий список
+        if anagrams not in anagram_groups:
+            anagram_groups.append(anagrams)
+
+    return anagram_groups
+
+
+# Тест для примеров и проверки:
+a = combine_anagrams(["cars", "for", "potatoes", "racs", "four",
+                      "scar", "creams", "scream"])
+
+print(a)
+
+# [ ["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"],
+# ["creams", "scream"] ]
