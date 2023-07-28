5e4cea623987b6897fb7fcf0234cf240e43c85a4
Motayassa <malovilyajob@gmail.com>
2023-07-28 16:49:43 +0300

    Add the ability to search for digits in "input" and tests

--- task_08.py
+++ task_08.py
@@ -13,15 +13,21 @@ def multiply_numbers(inputs=None):
 
     if inputs is None:
         return None
-        
+
+    if type(inputs) == list:
+        inputs = [str(x) for x in inputs]
+
+    if type(inputs) in (int, float):
+        inputs = str(inputs)
+
+    # Список, хранящий числа из input
+    lst_number = list(map(int, filter(lambda x: x.isdigit(), inputs)))
 
 
-'''
 # Тест для примеров и проверки:
-multiply_numbers() # => None
-multiply_numbers('ss') # => None
-multiply_numbers('1234') # => 24
-multiply_numbers('sssdd34') # => 12
-multiply_numbers(2.3) # => 6
-multiply_numbers([5, 6, 4]) # => 120
-'''
+multiply_numbers()  # => None
+multiply_numbers('ss')  # => None
+multiply_numbers('1234')  # => 24
+multiply_numbers('sssdd34')  # => 12
+multiply_numbers(2.3)  # => 6
+multiply_numbers([5, 6, 4])  # => 120
