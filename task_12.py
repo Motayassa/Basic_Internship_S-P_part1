90bc95e6a1642a2dc662c13451d8a990ac42f525
Motayassa <malovilyajob@gmail.com>
2023-07-29 14:36:25 +0300

    Start task_12.py

---
+++ task_12.py
@@ -0,0 +1,58 @@
+'''
+Создайте класс JellyBean, расширяющий класс Dessert (из Упражнения 11) новым
+геттером и сеттером для атрибута flavor (все параметры являются не
+обязательными). Измените метод is_delicious таким образом, чтобы он возвращал
+false только в тех случаях, когда flavor равняется «black licorice».
+
+'''
+
+
+class Dessert:
+    '''
+    Описывает дессерты свойствами - имя и калорийность, содержит два метода.
+
+    '''
+    def __init__(self, name=None, calories=0):
+        self.name = name
+        self.calories = calories
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name_dessert):
+        self._name = name_dessert
+
+    @property
+    def calories(self):
+        return self._calories
+
+    @calories.setter
+    def calories(self, calories):
+        self._calories = calories
+
+    def is_healthy(self):
+        '''возвращает true при условии калорийности десерта менее 200'''
+
+        if not self.calories < 200:
+            return False
+
+        return True
+
+    def is_delicious(self):
+        '''возвращает true для всех десертов'''
+
+        if not self.__class__ == Dessert:
+            return False
+
+        return True
+
+
+# Тесты
+a = Dessert()
+a = Dessert('name', 200)
+print(a.name)
+print(a.calories)
+a.is_healthy()
+a.is_delicious()
