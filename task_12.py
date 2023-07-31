ecbd42be22c0f32b8b62066a5b092f98665dd24d
Motayassa <malovilyajob@gmail.com>
2023-07-31 17:33:35 +0300

    Add tests

--- task_12.py
+++ task_12.py
@@ -80,7 +80,33 @@ class JellyBean(Dessert):
         return True
 
 
-# Тесты
-a = JellyBean('name', 100, 'black licorice')
-a.is_delicious()
-print(a.__dict__)
+# Test commands:
+dessert = JellyBean()
+if not issubclass(dessert.__class__, JellyBean): raise Exception("Invalid inheritance")
+dessert.name = "test_name"
+print(dessert.name)
+# test_name
+
+dessert.name = "test_name2"
+print(dessert.name)
+# test_name2
+
+if dessert.name != "test_name2": raise Exception("Setter for name is not working")
+dessert.calories = "test_calories"
+print(dessert.calories)
+# test_calories
+
+dessert.calories = "test_calories2"
+print(dessert.calories)
+# test_calories2
+
+if dessert.calories != "test_calories2": raise Exception("Setter for calories is not working")
+print(dessert.is_delicious())
+# True
+
+if not dessert.is_delicious(): raise Exception("Invalid method result")
+dessert.flavor = "test_flavor"
+print(dessert.flavor)
+# test_flavor
+
+print(dessert.is_healthy())
